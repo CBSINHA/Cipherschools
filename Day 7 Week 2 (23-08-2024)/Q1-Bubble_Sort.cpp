fdcb284e742ab00148bde80fa2f525fb176c23bc
Shashank Sinha <shashanksinha590@gmail.com>
2024-08-28 22:33:17 +0530

    Changed to correct bubble sort algorithm

--- Day 7 Week 2 (23-08-2024)/Q1-Bubble_Sort.cpp
+++ Day 7 Week 2 (23-08-2024)/Q1-Bubble_Sort.cpp
@@ -1,31 +1,28 @@
-/*Ques-01 Write C++ program that take array from the user and sort the array using bubble sort algorithm.
-Input:
-N = 5
-arr[] = {4, 1, 3, 9, 7}
-Output:
-1 3 4 7 9*/
+// /*Ques-01 Write C++ program that take array from the user and sort the array using bubble sort algorithm.
+// Input:
+// N = 5
+// arr[] = {4, 1, 3, 9, 7}
+// Output:
+// 1 3 4 7 9*/
 #include<iostream>
 using namespace std;
-int main(){
-    int n;
-    cout<<"N = ";
+int main()
+{
+    int n,arr[100];
+    cout<<"N: ";
     cin>>n;
-    int arr[1000];
-    cout<<"arr[] = ";
+    cout<<"arr[]: ";
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]>arr[j]){
-                int temp=arr[j];
-                arr[j]=arr[i];
-                arr[i]=temp;
-            }
+    for(int i=n-1;i>=1;i--){
+        for(int j=0;j<=i-1;j++){
+            if(arr[j]>arr[j+1])
+            swap(arr[j],arr[j+1]);
         }
     }
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    return 0;
+    cout<<"Sorted array using bubble sort is: ";
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+ return 0;
 }
